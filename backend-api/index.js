451fd87714581becadd358d9caa6cc1bc71a4bca
SandyaNW <rafiopang@gmail.com>
2026-07-25 22:27:59 +0700

    erorrrr.....

--- backend-api/index.js
+++ backend-api/index.js
@@ -108,7 +108,12 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (
+      !origin || 
+      allowedOrigins.includes(origin) || 
+      origin.endsWith('.vercel.app') || 
+      /^https:\/\/.*\.vercel\.app$/.test(origin)
+    ) {
       callback(null, true);
     } else {
       callback(new Error(`CORS Policy Error: Origin ${origin} not allowed`));
